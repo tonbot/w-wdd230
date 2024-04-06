f372b080d8c3bf3a603b04f0c4046fc06c3725db
tonbot <toy505050@yahoo.com>
2024-04-06 14:10:02 +0100

    new commit

--- Scoots/scripts/main.js
+++ Scoots/scripts/main.js
@@ -16,7 +16,7 @@ function openNav() {
 
   function closeMessage() {
     document.getElementById("closeable-message").style.display="none";
-    document.getElementById("nav-toggle").style.top="62px";
+    document.getElementById("nav-toggle").style.top="75px";
   }
 
  
